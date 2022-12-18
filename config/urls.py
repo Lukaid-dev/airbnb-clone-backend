b72ca7a092d58e3742282bb5ea08debf43b94a8f
SWLee <swlee@SWLees-MacBook-Air.local>
2022-12-19 01:00:19 +0900

    update

--- config/urls.py
+++ config/urls.py
@@ -14,11 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
-from rooms import views as room_views
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("rooms/", room_views.test_hello),
+    path("rooms/", include("rooms.urls")),
 ]
--- rooms/views.py
+++ rooms/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def test_hello(request):
-    return HttpResponse("Hello World")
+def see_all_rooms(request):
+    return HttpResponse("see all rooms")
