c0d825b2ecadf8e0736034a81923fb53e3df3238
SWLee <swlee@SWLees-MacBook-Air.local>
2023-01-18 00:52:57 +0900

    drf

--- categories/views.py
+++ categories/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.exceptions import NotFound
+from rest_framework.status import HTTP_204_NO_CONTENT
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Category
+from .serializers import CategorySerializer
+
+
+@api_view(["GET", "POST"])
+def categories(request):
+    if request.method == "GET":
+        all_category = Category.objects.all()
+        serializer = CategorySerializer(all_category, many=True)
+
+        return Response(serializer.data)
+    elif request.method == "POST":
+        serializer = CategorySerializer(data=request.data)
+        if serializer.is_valid():
+            # serializer의 create method를 찾음
+            new_category = serializer.save()
+            return Response(CategorySerializer(new_category).data)
+        else:
+            return Response(serializer.errors, status=400)
+
+
+@api_view(["GET", "PUT", "DELETE"])
+def category(request, pk):
+    try:
+        category = Category.objects.get(pk=pk)
+    except Category.DoesNotExist:
+        raise NotFound
+
+    if request.method == "GET":
+        serializer = CategorySerializer(category)
+        return Response(serializer.data)
+
+    elif request.method == "PUT":
+        serializer = CategorySerializer(
+            category,
+            data=request.data,
+            partial=True,
+        )
+        if serializer.is_valid():
+            # serializer에 instance를 같이 넘겨주면 update method를 찾음
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=400)
+
+    elif request.method == "DELETE":
+        category.delete()
+        return Response(status=HTTP_204_NO_CONTENT)
