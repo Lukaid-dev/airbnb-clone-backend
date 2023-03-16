f8eaa642e4c8d45025c35d405578a11c4089167c
Seongwoo_Lukaid <seongwoo.lee@jjaann.com>
2023-03-17 00:16:17 +0900

    edit csrf

--- config/settings.py
+++ config/settings.py
@@ -178,4 +178,4 @@ CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:3000", "http://localhost:3000", "http:
 
 CORS_ALLOW_CREDENTIALS = True
 
-CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:3000"]
+CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1:3000"]
